8255c27aabd07d391f6ee343c9ab9865e363945e
vladimir-yu-fedorov <vladimir.fedorov@raiden-numerics.com>
2025-07-03 18:20:44 +0900

    Generate mesh on the fly

--- Electromagnetics/Ren_2014_MEMS_Comb_Drive/case.py
+++ Electromagnetics/Ren_2014_MEMS_Comb_Drive/case.py
@@ -1,3 +1,5 @@
+import gmsh
+
 from mufem import (
     Bnd,
     Vol,
@@ -14,6 +16,86 @@ from mufem.electromagnetics.electrostatics import (
 )
 
 
+def create_geometry(xshift, mesh_file="geometry.msh"):
+    gmsh.initialize()
+
+    # Create geometry ------------------------------------------------------------------
+    u = 1  # unit of spatial dimensions
+
+    def tooth(x, y, z):
+        return gmsh.model.occ.addBox(x, y, z, 15 * u, 4 * u, 4 * u)
+
+    # Comb 1:
+    tag_tooth1 = tooth(0, 0, 0)
+    tag_tooth2 = tooth(0, 0, 10 * u)
+    tag_tooth3 = tooth(0, 0, 20 * u)
+    tag_tooth4 = tooth(0, 0, 30 * u)
+    tag_base = gmsh.model.occ.addBox(0, 0, 0, 5 * u, 4 * u, 34 * u)
+
+    ov = gmsh.model.occ.fuse(
+        [(3, tag_base)],
+        [(3, tag_tooth1), (3, tag_tooth2), (3, tag_tooth3), (3, tag_tooth4)],
+    )
+    comb1 = ov[0][0]
+
+    gmsh.model.occ.translate([comb1], -xshift / 2, 0, 0)
+
+    # Comb 2:
+    tag_tooth1 = tooth(7 * u, 0, 5 * u)
+    tag_tooth1 = tooth(7 * u, 0, 5 * u + 10 * u)
+    tag_tooth1 = tooth(7 * u, 0, 5 * u + 20 * u)
+    tag_base = gmsh.model.occ.addBox(17 * u, 0, 0, 5 * u, 4 * u, 34 * u)
+
+    ov = gmsh.model.occ.fuse(
+        [(3, tag_base)], [(3, tag_tooth1), (3, tag_tooth2), (3, tag_tooth3)]
+    )
+    comb2 = ov[0][0]
+
+    gmsh.model.occ.translate([comb2], +xshift / 2, 0, 0)
+
+    # Domain box:
+    wx = 88 * u
+    wy = 44 * u
+    wz = 88 * u
+    tag_domain = gmsh.model.occ.addBox(
+        11 * u - wx / 2, -6 * u, 17 * u - wz / 2, wx, wy, wz
+    )
+
+    ov = gmsh.model.occ.cut([(3, tag_domain)], [comb1, comb2])
+    domain = ov[0][0]
+
+    gmsh.model.occ.synchronize()
+
+    # Assign attributes ----------------------------------------------------------------
+    print("3D: ", gmsh.model.getEntities(3))
+    print("2D: ", gmsh.model.getEntities(2))
+
+    comb1 = [(2, i) for i in range(1, 19)]
+    comb2 = [(2, i) for i in range(19, 37)]
+    ground = [(2, 38)]
+    domain = [(3, 3)]
+
+    gmsh.model.addPhysicalGroup(2, [dimTag[1] for dimTag in comb1], name="Comb1", tag=1)
+    gmsh.model.addPhysicalGroup(2, [dimTag[1] for dimTag in comb2], name="Comb2", tag=2)
+    gmsh.model.addPhysicalGroup(
+        2, [dimTag[1] for dimTag in ground], name="Ground", tag=3
+    )
+    gmsh.model.addPhysicalGroup(
+        3, [dimTag[1] for dimTag in domain], name="Domain", tag=1
+    )
+
+    # Generate mesh --------------------------------------------------------------------
+    maxh = 4 * u
+    gmsh.option.setNumber("Mesh.MeshSizeMax", maxh)
+
+    gmsh.model.mesh.generate(3)
+
+    gmsh.option.setNumber("Mesh.MshFileVersion", 2.2)
+    gmsh.write(mesh_file)
+
+    gmsh.finalize()
+
+
 sim = Simulation.New(name="Ren_2014_MEMS_Comb_Drive")
 
 runner = SteadyRunner(total_iterations=2)
@@ -23,9 +105,10 @@ sim.set_runner(runner)
 # Mesh ---------------------------------------------------------------------------------
 xshifts = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8]
 
-mesh_files = [f"geometry_xshift={x:.1f}.msh" for x in xshifts]
+if sim.get_machine().is_main_process():
+    create_geometry(xshifts[0])
 
-sim.get_domain().load_mesh(mesh_files[0])
+sim.get_domain().load_mesh("geometry.msh")
 sim.get_domain().get_mesh().scale(1e-6)
 
 refinement_model = RefinementModel()
@@ -90,8 +173,11 @@ with open("results/Capacitance.csv", "w") as fp:
 vis = sim.get_field_exporter()
 vis.add_field_output("Electric Potential")
 
-for ix, mesh_file in enumerate(mesh_files):
-    sim.get_domain().load_mesh(mesh_file)
+for xshift in xshifts:
+    if sim.get_machine().is_main_process():
+        create_geometry(xshift)
+
+    sim.get_domain().load_mesh("geometry.msh")
     sim.get_domain().get_mesh().scale(1e-6)
 
     for i in range(max_iterations):
@@ -106,7 +192,7 @@ for ix, mesh_file in enumerate(mesh_files):
 
         if sim.get_machine().is_main_process():
             with open("results/Capacitance.csv", "a") as fp:
-                fp.write(f"{xshifts[ix]:.1f}, {ncells}, {capacitance}\n")
+                fp.write(f"{xshift:.1f}, {ncells}, {capacitance}\n")
 
         if ncells >= max_ncells:
             break
