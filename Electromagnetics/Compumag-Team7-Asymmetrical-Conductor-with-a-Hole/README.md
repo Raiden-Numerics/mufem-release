bf7eafcb5f84b0b388d1bd4af864a359b91d3373
Paul Hilscher <paul.hilscher@raiden-numerics.com>
2025-08-08 09:21:45 +0900

    feat(DOC-24): Add Compumag TEAM-7 case (#34)

---
+++ Electromagnetics/Compumag-Team7-Asymmetrical-Conductor-with-a-Hole/README.md
@@ -0,0 +1,66 @@
+# Compumag Team 7: Asymmetrical Conductor with Hole
+
+## Introduction
+
+
+In the Compumag TEAM Problem 7[1] a thick aluminum plate with a hole is placed below an excitation
+coil with a sinusoidal excitation. 
+
+<div align="center">
+<img src="./data/Geometry.png" alt="drawing" width="600">
+</div>
+<div align="center">
+<em>Figure 1: The geometry of the benchmark. A coil is placed above an aluminium plate with a hole.</em>
+</div>
+<br /><br />
+
+Relevant models:
+
+* Time-Harmonic Magnetics
+* Excitation Coil
+
+
+## Problem Description
+
+The aluminium plate has conductivity of $\sigma=3.526 \times 10^7$. The stranded coil is excited with a coil current of 2742 AT with a sinusoidal current with a frequency of 50Hz (reference values of 200Hz also exist.). The results are compared to measurements of the magnetic flux density at various
+positions.
+
+We use the reference solution provided by Fujiwara et al.[2]. 
+
+## Setup
+
+We use third order solution. Note however that only a second order accurate geometry is used.
+
+## Results
+
+* **Magnetic Flux Density**
+
+  We compare the calculated results with the results of the magnetic flux density we the provided results by [2]
+
+  | Magnetic Flux Density A1-B1      |  Magnetic Flux Density A2-B2  |
+  | -------------------------------- | -------------------------------- |
+  |  <img src="./results/Magnetic_Flux_Density_Bz_A1-B1.png" alt="drawing" width="600"> | <img src="./results/Magnetic_Flux_Density_Bz_A2-B2.png" alt="drawing" width="600">  |
+
+  We observe a very good fit for both quantities.
+
+
+## Scenes
+
+* **Magnetic Flux Density (Abs)**
+
+  <div align="center">
+  <img src="./results/Scene_MagneticFluxDensityAbs_0.png" alt="drawing" width="600">
+  </div>
+  <div align="center">
+  <em>The absolute value of the magnetic flux density. </em>
+  </div>
+
+
+
+
+## References
+
+[1] https://www.compumag.org/wp/wp-content/uploads/2018/06/problem7.pdf
+
+[2] Fujiwara K, Nakata T. Results for benchmark problem 7 (asymmetrical conductor with a hole). COMPEL-The international journal for computation and mathematics in electrical and electronic engineering. 1990 Mar 1;9(3):137-54.
+
---
+++ Electromagnetics/Compumag-Team7-Asymmetrical-Conductor-with-a-Hole/case.py
@@ -0,0 +1,159 @@
+import mufem
+from mufem.electromagnetics.coil import (
+    CoilExcitationCurrent,
+    CoilSpecification,
+    CoilTopologyClosed,
+    CoilTypeStranded,
+    ExcitationCoilModel,
+)
+from mufem.electromagnetics.timeharmonicmagnetic import (
+    TimeHarmonicMagneticGeneralMaterial,
+    TimeHarmonicMagneticModel,
+)
+
+from mufem import Vol
+
+import matplotlib.pyplot as plt
+import numpy
+from pathlib import Path
+
+
+dir_path = Path(__file__).resolve().parent
+
+
+sim = mufem.Simulation.New(name="Team-7", mesh_path=f"{dir_path}/geometry.mesh")
+
+# Setup Problem
+steady_runner = mufem.SteadyRunner(total_iterations=1)
+sim.set_runner(steady_runner)
+
+magnetic_model = TimeHarmonicMagneticModel(Vol.Everywhere, frequency=50, order=3)
+sim.get_model_manager().add_model(magnetic_model)
+
+# Define the materials
+air_material = TimeHarmonicMagneticGeneralMaterial.Constant(
+    name="Air", marker="Air" @ Vol
+)
+
+copper_material = TimeHarmonicMagneticGeneralMaterial.Constant(
+    name="Copper", marker="Coil" @ Vol
+)
+
+alu_material = TimeHarmonicMagneticGeneralMaterial.Constant(
+    name="Alu",
+    marker="Plate" @ Vol,
+    relative_magnetic_permeability=1.0,
+    electric_conductivity=3.526e7,
+)
+
+magnetic_model.add_materials([air_material, copper_material, alu_material])
+
+# Coil
+coil_model = ExcitationCoilModel()
+sim.get_model_manager().add_model(coil_model)
+
+coil_topology = CoilTopologyClosed(x=0.2, y=0.01, z=0.07, dx=1.0, dy=0.0, dz=0.0)
+coil_type = CoilTypeStranded(number_of_turns=2742)
+coil_excitation = CoilExcitationCurrent.Constant(1.0)
+
+coil = CoilSpecification(
+    name="Coil",
+    marker="Coil" @ Vol,
+    topology=coil_topology,
+    type=coil_type,
+    excitation=coil_excitation,
+)
+coil_model.add_coil_specification(coil)
+
+# Run the code and output
+
+
+sim.run()
+
+vis = sim.get_field_exporter()
+
+vis.add_field_output("MagneticFluxDensityReal")
+vis.add_field_output("MagneticFluxDensityImag")
+vis.add_field_output("MagneticFluxDensityAbs")
+
+vis.save(order=3)
+
+# Post Process Results
+probe_reports = [("A1-B1", 0.072), ("A2-B2", 0.144)]
+x_values = numpy.linspace(start=0.0, stop=0.288, num=64)
+
+
+for probe in probe_reports:
+
+    b_values = []
+
+    for x in x_values:
+
+        magnetic_flux_density_real_report = mufem.ProbeReport.SinglePoint(
+            name="MagneticFluxDensityRealReport",
+            cff_name="MagneticFluxDensityReal",
+            x=x,
+            y=probe[1],
+            z=0.034,
+        )
+
+        magnetic_flux_density_imag_report = mufem.ProbeReport.SinglePoint(
+            name="MagneticFluxDensityImagReport",
+            cff_name="MagneticFluxDensityImag",
+            x=x,
+            y=probe[1],
+            z=0.034,
+        )
+
+        b_real = magnetic_flux_density_real_report.evaluate()
+        b_imag = magnetic_flux_density_imag_report.evaluate()
+
+        # We multiply by 1e3 to convert from T to mT
+        # and by 1e3 to convert from m to mm
+        b_values.append((1e3 * x, 1e3 * (-b_real.z + 1j * b_imag.z)))
+
+    # Plot
+    # flake8: noqa: FKA100
+
+    plt.clf()
+
+    ref = numpy.loadtxt(
+        f"{dir_path}/data/Bz_{probe[0]}.csv", delimiter=",", comments="#"
+    )
+
+    plt.plot(
+        ref[:, 1],
+        1.0e-1 * ref[:, 2],
+        "ko-",
+        label="Reference",
+        markersize=6.5,
+    )
+
+    plt.plot(
+        [x_value for x_value, _ in b_values],
+        [numpy.real(b_value) for _, b_value in b_values],
+        label=f"real",
+        color="r",
+    )
+
+    plt.plot(
+        ref[:, 1],
+        1.0e-1 * ref[:, 3],
+        "ko-",
+    )
+
+    plt.plot(
+        [x_value for x_value, _ in b_values],
+        [numpy.imag(b_value) for _, b_value in b_values],
+        label=f"imag",
+        color="b",
+    )
+
+    plt.xlabel("x [mm]")
+    plt.ylabel("B [mT]")
+    plt.title(f"Magnetic Flux Density at {probe[0]}")
+
+    plt.savefig(
+        f"{dir_path}/results/Magnetic_Flux_Density-{probe[0]}.png",
+        bbox_inches="tight",
+    )
